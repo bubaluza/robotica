a35a1cb7abeddd70e45bc78b87d53f2ccc502450
Bubaluza <gabrielschiller8@gmail.com>
2019-04-16 02:23:01 -0300

    upgrade stepper.ino

--- stepper/bouceStepper/serialStepper/serialStepper.ino
+++ stepper/bouceStepper/serialStepper/serialStepper.ino
@@ -2,9 +2,16 @@
     and modified by Gabrel Schiller - https://github.com/bubaluza
 */
 #include <AccelStepper.h>
+#define snapAction1 4   //choice the INPUT PIN
+#define snapAction2 5   //choice the INPUT PIN
 
-boolean completeInfo = false;
+bool completeInfo = false;
+bool calibrate = false; 
 int xDisplacement = 0;
+long pathBegin = 0;
+long pathEnd = 0;
+float realPathSize = 0;
+
 
 AccelStepper stepperX(AccelStepper::DRIVER, 0, 2); // (step, dir) Defaults to AccelStepper::DRIVER (2 pins) on 2, 3
 
@@ -12,10 +19,31 @@ void setup() {
   Serial.begin(9600);
   stepperX.setMaxSpeed(250);
   stepperX.setAcceleration(1000);
+  pinMode(snapAction1, INPUT_PULLUP);
+  pinMode(snapAction2, INPUT_PULLUP);
 }
 
 void loop() {
   ESPserialEvent();
+  if(calibrate){
+    stepperX.moveTo(-20000);
+    while(digitalRead(snapAction1) && digitalRead(snapAction2)){
+      stepperX.run();
+    }
+    stepperX.setCurrentPosition(0);
+    stepperX.move(800);
+    stepperX.runToPosition();
+    stepperX.moveTo(20000);
+    while(digitalRead(snapAction1) && digitalRead(snapAction2)){
+      stepperX.run();
+    }
+    pathEnd = stepperX.currentPosition();
+    stepperX.move(-800);
+    stepperX.runToPosition();
+    calibrate = false;
+    Serial.println("calibrate finished");
+  }
+  
   if (completeInfo) {
     stepperX.moveTo(xDisplacement);
     completeInfo = false;
@@ -50,6 +78,14 @@ void ESPserialEvent() {
         xDisplacement += ((angle/1.8));
         Serial.print("Angle: ");
         Serial.println(angle);
+        break;
+      case 'c':
+        realPathSize = Serial.parseFloat();     // mm
+        calibrate = true;
+        Serial.println("calibrate on");
+      case 'm':
+        int mmMove = Serial.parseInt();
+        xDisplacement += (mmMove/(pathEnd/realPathSize))
         break;
     }
     completeInfo = c == '\n';
